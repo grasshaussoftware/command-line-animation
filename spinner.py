fe8da3d7d823a383c1b092557a50e7f8aa66a0b7
deus.opus <deusopus@gmail.com>
2026-04-16 08:58:06 -0700

    Implement sleep_with_spinner function
    Refactor sleep function to include spinner animation.

--- spinner.py
+++ spinner.py
@@ -1,16 +1,24 @@
-import time
+from itertools import cycle
 from time import sleep
 
-#delay between actions
-            print('Sleeping ', end='', flush=True)
 
-            for x in range(250):
-                for frame in r'-\|/-\|/':
-                # Back up one character then print our next frame in the animation
-                    print('\b', frame, sep='', end='', flush=True)
-                    sleep(0.2)
+def sleep_with_spinner(seconds: float = 400.0, message: str = "Sleeping"):
+    """
+    Show a nice spinner animation while sleeping for a given number of seconds.
+    Much cleaner and reusable than the original snippet.
+    """
+    print(f'{message} ', end='', flush=True)
 
-                # Back up one character, print a space to erase the spinner, then a newline
-                # so that the prompt after the program exits isn't on the same line as our
-                # message
-            print('\b ')
+    # Standard spinner frames (cycles forever)
+    spinner = cycle(r'-\|/')
+
+    # Calculate exact number of animation frames needed
+    frames_needed = int(seconds / 0.2)
+
+    for _ in range(frames_needed):
+        # Overwrite the previous spinner character in place
+        print('\b', next(spinner), sep='', end='', flush=True)
+        sleep(0.2)
+
+    # Erase the spinner character and move to the next line
+    print('\b ', flush=True)
